129cb88065739b52e948c24d4f985640ed9abc8e
lemaiyan <george.kamunya@gmail.com>
2020-04-03 07:25:15 +0300

    Union: add union types

--- schema.py
+++ schema.py
@@ -49,6 +49,11 @@ class Race(SQLAlchemyObjectType):
         return query.get(id)
 
 
+class SearchResult(graphene.Union):
+    class Meta:
+        types = (Crew, Ship, Rank, Race)
+
+
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     all_crew = SQLAlchemyConnectionField(Crew)
@@ -58,6 +63,7 @@ class Query(graphene.ObjectType):
     in_race = graphene.List(Crew, race=graphene.String())
     in_rank = graphene.List(Crew, rank=graphene.String())
     ship_crew = graphene.List(Crew, ship=graphene.String())
+    search = graphene.List(SearchResult, q=graphene.String())
 
     @staticmethod
     def resolve_in_race(parent, info, **args):
@@ -77,8 +83,23 @@ class Query(graphene.ObjectType):
     def resolve_ship_crew(parent, info, **args):
         ship = args.get('ship').lower()
         query = Crew.get_query(info).join(ShipModel, CrewModel.ship).filter(
-           ShipModel.name.ilike(f'%{ship}%'))
+            ShipModel.name.ilike(f'%{ship}%'))
         return query.all()
+
+    @staticmethod
+    def resolve_search(parent, info, **args):
+        q = args.get('q').lower()
+        rank_query = Rank.get_query(info)
+        race_query = Race.get_query(info)
+        ship_query = Ship.get_query(info)
+        crew_query = Crew.get_query(info)
+
+        rank = rank_query.filter(RankModel.name.contains(q)).all()
+        race = race_query.filter(RaceModel.name.contains(q)).all()
+        ship = ship_query.filter(ShipModel.name.contains(q)).all()
+        crew = crew_query.filter(CrewModel.name.contains(q)).all()
+
+        return rank + race + ship + crew
 
 
 schema = graphene.Schema(query=Query)
