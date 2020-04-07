d82e2450fe8021a3be703a3e920f21d3a1178fbc
lemaiyan <george.kamunya@gmail.com>
2020-04-07 09:46:38 +0300

    Update the logging and README.md

--- README.md
+++ README.md
@@ -9,3 +9,122 @@ the steps below
 
 * Run `docker-compose up` to start the service and wait for the app to start
 * Go to your favorite GraphQL client and connect to http://localhost:5000/graphql and start writing queries
+
+## Queries
+
+Below are sample queries you can run.
+```graphql
+query getAllShips{
+  allShip{
+    edges{
+      node{
+        name
+      }
+    }
+  }
+}
+```
+
+```graphql
+query crewDetails {
+  allCrew(last: 10) {
+    pageInfo {
+      hasNextPage
+      hasPreviousPage
+      endCursor
+      startCursor
+    }
+    edges {
+      node {
+        name
+        ship {
+          name
+        }
+        race {
+          name
+        }
+        rank {
+          name
+        }
+      }
+      cursor
+    }
+  }
+}
+```
+
+```graphql
+{
+  shipCrew(ship: "enterprise"){
+    name,
+    ship{
+      name
+    }
+    rank{
+      name
+    }
+    race {
+      name
+    }
+  }
+}
+```
+```graphql
+query search{
+  search(q: "c") {
+    ... on Crew {
+      name
+    }
+    ... on Race {
+      name
+    }
+    ... on Rank {
+      name
+    }
+    ... on Ship {
+      name
+    }
+  }
+}
+```
+
+## Mutations 
+Below are some sample mutations
+```graphql
+mutation{
+  addShip(shipData: {name: "U.S.S. Shenzhou"}){
+    ship{
+      name
+      dateAdded
+    }
+  }
+}
+```
+
+```graphql
+mutation {
+  addCrew(
+    crewData: {
+      name: "Philippa Georgiou"
+      ship: "U.S.S. Shenzhou"
+      rank: "Captain"
+      race: "Human"
+    }
+  ) {
+    name
+    ship
+    rank
+    race
+  }
+}
+```
+
+```graphql
+mutation {
+  addRace(raceData: { name: "Bojorian" }) {
+    race {
+      name
+    }
+  }
+}
+```
--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from database import db_session, init_db
-from schema import schema
-
-from graphql_server import (HttpQueryError, default_format_error,
+import structlog
+from graphql_server import (default_format_error,
                             encode_execution_results, json_encode,
                             load_json_body, run_http_query)
+
+from database import init_db
+from schema import schema
+
+logger = structlog.get_logger()
 
 
 class App():
@@ -11,7 +14,9 @@ class App():
         init_db()
 
     def query(self, request):
+        logger.info("begin-query")
         data = self.parse_body(request)
+        logger.info("query", request=request)
         execution_results, params = run_http_query(
             schema,
             'post',
@@ -19,19 +24,24 @@ class App():
         result, status_code = encode_execution_results(
             execution_results,
             format_error=default_format_error, is_batch=False, encode=json_encode)
+        logger.info("query", result=result, status_code=status_code)
+        logger.info("end-query")
         return result
 
     def parse_body(self, request):
-        # We use mimetype here since we don't need the other
-        # information provided by content_type
+        logger.info('start-parse-body')
         content_type = request.mimetype
+        query = None
         if content_type == 'application/graphql':
-            return {'query': request.data.decode('utf8')}
+            query = {'query': request.data.decode('utf8')}
 
         elif content_type == 'application/json':
-            return load_json_body(request.data.decode('utf8'))
+            query = load_json_body(request.data.decode('utf8'))
 
         elif content_type in ('application/x-www-form-urlencoded', 'multipart/form-data'):
-            return request.form
+            query = request.form
 
-        return {}
+        logger.info('parse-body', query=query, content_type=content_type)
+        logger.info('end-parse-body')
+
+        return query
